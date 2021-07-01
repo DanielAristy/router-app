ffeb757f03b96f6c01ebccec57a191fd7a4946f9
DanielAristy <danielaristy22@gmail.com>
2021-06-30 22:54:18 -0500

    Manejo de Navbar con Link en el cual se hace que cuando se seleccione algunos de estos valores no se recarge la pagina

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,10 +3,12 @@ import About from '../components/About'
 import Contact from '../components/Contact'
 import Home from '../components/Home'
 import NotFound from '../components/NotFound'
+import Navbar from '../components/Navbar'
 
 export default function AppRouter() {
     return (
         <Router>
+            <Navbar />
             <Switch>
                 <Route path="/about">
                     <About />
