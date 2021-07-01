fed71d0b4f1e96794e44691bfd53171ba4acbf8d
DanielAristy <danielaristy22@gmail.com>
2021-07-01 12:20:00 -0500

    Se ocultan algunos elementos del menu ya que se van a mostrar Login y Register solo cuando el usuario no este logeado y cuando lo este se oculta y se muestran otros enlaces como Dashboard y Payments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,21 +18,30 @@ export default function Navbar() {
                 <li>
                     <NavLink exact to="/categories" activeClassName="active">Categories</NavLink>
                 </li>
-                <li>
-                    <NavLink exact to="/login" activeClassName="active">Login</NavLink>
-                </li>
-                <li>
-                    <NavLink exact to="/register" activeClassName="active">Register</NavLink>
-                </li>
-                <li>
-                    <NavLink exact to="/dashboard" activeClassName="active">Dashboard</NavLink>
-                </li>
-                <li>
-                    <NavLink exact to="/payments" activeClassName="active">Payments</NavLink>
-                </li>
-                <li>
-                    <button onClick={auth.logout}>Logout</button>
-                </li>
+                {!auth.isLogged() && (
+                    <>
+                        <li>
+                            <NavLink exact to="/login" activeClassName="active">Login</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to="/register" activeClassName="active">Register</NavLink>
+                        </li>
+                    </>
+                )}
+                {auth.isLogged() && (
+                    <>
+                        <li>
+                            <NavLink exact to="/dashboard" activeClassName="active">Dashboard</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to="/payments" activeClassName="active">Payments</NavLink>
+                        </li>
+                        <li>
+                            <button onClick={auth.logout}>Logout</button>
+                        </li>
+                    </>
+                )}
+                
             </ul>
         </nav>
     )
