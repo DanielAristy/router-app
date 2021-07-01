c8f7eb4e0677599778cd98dfd10c2afb8d95f2cf
DanielAristy <danielaristy22@gmail.com>
2021-07-01 15:36:58 -0500

    aplicando un tercer nivel de rutas en el que se aplica useRouteMatch para tener las rutas anteriores y utlizarlas

--- src/routers/CategoriesRouter.js
+++ src/routers/CategoriesRouter.js
@@ -1,4 +1,4 @@
-import {  Route, Switch, Redirect, NavLink, useRouteMatch } from 'react-router-dom'
+import {  Route, Switch, Redirect, NavLink, useRouteMatch, Link } from 'react-router-dom'
 import Categories from '../components/Categories'
 import NotFound from '../components/NotFound'
 import PrivateRoute from '../routers/PrivateRoute'
@@ -23,16 +23,12 @@ export default function CategoriesRouter(){
                 </li>
             </ul>
             <Switch>
-                <Route exact path="/categories" component={Categories} />
-                <PrivateRoute
-                 exact
-                  path="/categories/terror"
-                   component={() => <h1>Categories Terror</h1>} />
-        
-                <Route exact path="/categories/action">
+                <Route exact path={`${url}`} component={Categories} />
+                <PrivateRoute path={`${url}/terror`} component={TerrorRouter} />
+                <Route exact path={`${url}/action`}>
                     <h1>Categories Action</h1>
                 </Route>
-                <Route exact path="/categories/anime">
+                <Route exact path={`${url}/anime`}>
                     <h1>Categories Anime</h1>
                 </Route>
                 <Route path="*" component={NotFound}>
@@ -42,4 +38,39 @@ export default function CategoriesRouter(){
         </div>
         
     )
+}
+
+function TerrorRouter(){
+    //En este caso de utliza para la ruta /categories/terror
+    const {url} = useRouteMatch();
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to={`${url}`}>All</Link>
+                </li>
+                <li>
+                    <Link to={`${url}/gore`}>Gore</Link>
+                </li>
+                <li>
+                    <Link to={`${url}/suspense`}>Suspense</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route exact path={`${url}`}>
+                    <h3>Category terror 3</h3>
+                </Route>
+        
+                <Route exact path={`${url}/gore`}>
+                    <h3>Gore</h3>
+                </Route>
+                <Route exact path={`${url}/suspense`}>
+                    <h3>Suspense</h3>
+                </Route>
+                <Route path="*">
+                        <Redirect to="/404" />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
