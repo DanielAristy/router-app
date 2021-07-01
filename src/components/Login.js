4de1eac7f2a341093145d85874043f98a25add50
DanielAristy <danielaristy22@gmail.com>
2021-07-01 11:29:28 -0500

    Se implementa que obtenga la ruta anterior con el uso de useLocation extrayendo el estado que guarda

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,15 @@
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 import useAuth from '../auth/UseAuth'
 
 export default function Login() {
     const history = useHistory();
+    const location = useLocation();
+    const previousObjectURL = location.state?.from;
+
     const auth = useAuth();
     const handleLogin = () => {
         auth.login()
-        history.push("/dashboard")
+        history.push(previousObjectURL || "/dashboard")
     }
     return (
         <div>
--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,15 +1,20 @@
-import { Route, Redirect } from 'react-router-dom'
+import { Route, Redirect, useLocation} from 'react-router-dom'
 import UseAuth from '../auth/UseAuth';
 
 // const user = null;
-const user = {id: 1, username: 'Daniel22'};
+// const user = {id: 1, username: 'Daniel22'};
 
 export default function PrivateRoute({component: Component, ...rest}) {
     const auth = UseAuth();
+    const location = useLocation();
     return (
         // <Route exact={props.exact} path={props.path} component={props.component} />
         <Route {...rest}>
-            {auth.isLogged() ? (<Component />) : (<Redirect to="/login" />)}
+            {auth.isLogged() ?
+             (<Component />
+             ) : (
+             <Redirect to={{ pathname: "/login", state: { from: location }}} />
+             )}
             
         </Route>
     )
