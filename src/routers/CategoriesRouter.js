d31a4d8e9ac626748e0332290b6fcdf9813caec6
DanielAristy <danielaristy22@gmail.com>
2021-07-01 15:01:18 -0500

    Manejo del hooks useRouteMatch para obtener la ruta anterior en este caso usando rutas anidadas

--- src/routers/CategoriesRouter.js
+++ src/routers/CategoriesRouter.js
@@ -1,23 +1,25 @@
-import {  Route, Switch, Redirect, NavLink } from 'react-router-dom'
+import {  Route, Switch, Redirect, NavLink, useRouteMatch } from 'react-router-dom'
 import Categories from '../components/Categories'
 import NotFound from '../components/NotFound'
 import PrivateRoute from '../routers/PrivateRoute'
 
 export default function CategoriesRouter(){
+    //Guardar la ruta anterior en este caso /categories
+    const {url} = useRouteMatch();
     return (
         <div>
             <ul>
                 <li>
-                    <NavLink exact to="/categories" activeClassName="active">All</NavLink>
+                    <NavLink exact to={`${url}`} activeClassName="active">All</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/categories/terror" activeClassName="active">Terror</NavLink>
+                    <NavLink to={`${url}/terror`} activeClassName="active">Terror</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/categories/action" activeClassName="active">Action</NavLink>
+                    <NavLink to={`${url}/action`} activeClassName="active">Action</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/categories/anime" activeClassName="active">Anime</NavLink>
+                    <NavLink to={`${url}/anime`} activeClassName="active">Anime</NavLink>
                 </li>
             </ul>
             <Switch>
